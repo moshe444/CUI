f0aba28e64b9ecea35049273581f1de6ba5423b2
Conjee <shishiv30@live.cn>
2016-11-20 10:09:32 +0800

    Updates

--- dist/src/cui.js
+++ dist/src/cui.js
@@ -13550,18 +13550,6 @@ window.context = {};
 
 (function($) {
     $.CUI = {
-        defaultContext: {
-            $element: null,
-            name: '',
-            defaultOpt: null,
-            initBefore: null,
-            init: null,
-            exports: {},
-            setOptionsBefore: null,
-            setOptionsAfter: null,
-            destroyBefore: null,
-            initAfter: null,
-        },
         plugin: function(pluginContext) {
             var name = pluginContext.name;
             if ($.fn[name]) {
@@ -13571,17 +13559,28 @@ window.context = {};
 
             $.fn[name] = function(options) {
                 var $this = $(this);
-                if (name && $this.data(name)) {
+                var cache = $this.data(name);
+                if (cache && typeof (cache) !== 'string') {
                     if (options) {
-                        $this.data(name).setOptions(options);
+                        cache.setOptions && cache.setOptions(options);
                     }
-                    return $this.data(name);
+                    return cache;
                 }
 
                 //initial context of plugin
-                var context = $.extend(true, {
+                var context = $.extend({
                     $element: $this,
-                }, $.CUI.defaultContext, pluginContext);
+                    name: '',
+                    defaultOpt: null,
+                    initBefore: null,
+                    init: null,
+                    exports: {},
+                    setOptionsBefore: null,
+                    setOptionsAfter: null,
+                    destroyBefore: null,
+                    initAfter: null,
+                    isThirdPart: false,
+                }, pluginContext);
 
                 context.options = options;
                 context.$element = $this;
@@ -13606,43 +13605,48 @@ window.context = {};
             var that = this;
             return function(options) {
                 //before set options
-                $.proxy(context.setOptionsBefore, that)(context, options);
+                context.setOptionsBefore && $.proxy(context.setOptionsBefore, that)(context, options);
 
-                context.opt = $.extend(true, {}, context.opt, options);
+                context.opt = $.extend(context.opt, options);
 
                 //after set options
-                $.proxy(context.setOptionsAfter, that)(context, options);
+                context.setOptionsAfter && $.proxy(context.setOptionsAfter, that)(context, options);
             };
         },
         handleInit: function(context) {
             var that = this;
             var opt = context.opt;
+            var exports = context.exports;
             //before plugin initial event
             $.CUI.addEvent('cui.init.before.' + context.name, context);
-            opt.initBefore && $.CUI.addEvent(opt.initBefore, context);
+            opt.initbefore && $.CUI.addEvent(opt.initbefore, context);
 
             //before plugin initial custom event
             context.initBefore && $.CUI.addEvent(context.initBefore, context);
 
             context.init && $.proxy(context.init, that)(context);
 
-            //add exports for the plugin
-            $.proxy($.CUI.handleExports, that)(context);
+            //is third part plugin
+            if (context.isThirdPart && context.exports.original) {
+                context.exports.original = $.isFunction(context.exports.original) ? $.proxy(context.exports.original, context)() : context.exports.original;
+            } else {
+                //add exports for the plugin
+                $.proxy($.CUI.handleExports, that)(context);
+                //initial get options of plugin
+                context.exports.getOptions = function() {
+                    return opt;
+                };
 
-            //initial get options of plugin
-            context.exports.getOptions = function() {
-                return context.opt;
-            };
+                //initial set options of plugin
+                context.exports.setOptions = $.proxy($.CUI.handleOptions, that)(context);
 
-            //initial set options of plugin
-            context.exports.setOptions = $.proxy($.CUI.handleOptions, that)(context);
-
-            //destroy export for the plugin
-            context.exports.destroy = $.proxy($.CUI.handleDestroy, that)(context);
-
+                //destroy export for the plugin
+                context.exports.destroy = $.proxy($.CUI.handleDestroy, that)(context);
+            }
+            console.log(context.name);
             //after plugin initial custom event
             context.initAfter && $.proxy(context.initAfter, that)(context);
-            opt.initAfter && $.CUI.addEvent(opt.initAfter, context);
+            opt.initafter && $.CUI.addEvent(opt.initafter, context);
 
             //after plugin initial event
             $.CUI.addEvent('cui.init.after.' + context.name, context);
@@ -13670,10 +13674,13 @@ window.context = {};
         },
         addEvent: function(name, context) {
             if ($.isFunction(name)) {
-                name.apply(this, context.$element, context.opt);
+                name.apply(this, [context.$element, context.exports]);
             } else {
-                $(document).trigger(name, [context.$element, context.opt]);
+                $(document).trigger(name, [context.$element, context.exports]);
             }
+        },
+        loadJs: function() {
+
         }
     };
 })(jQuery);
@@ -13848,7 +13855,15 @@ if (!Number.isNaN) {
     var dataTableConfig = {
         $element: null,
         name: 'datatable',
-        defaultOpt: null,
+        defaultOpt: {
+            columns: [],
+            data: null,
+            maxcount: -1,
+            onbefore: null,
+            onafter: null,
+            defaultTemplate: null,
+            hideText: 'See More'
+        },
         initBefore: null,
         init: function(context) {
             var opt = context.opt;
@@ -13986,24 +14001,22 @@ if (!Number.isNaN) {
                 }
                 return $tfoot;
             };
-            var _initTable = context._initTable = function(isInital) {
-                if (isInital) {
+            var _initTable = context._initTable = function() {
+                //todo send context to the follow method, otherwise the context.opt will not update
+                if (opt.data && opt.data.length) {
                     $table = $('<table class="datatable"></table>');
                     $table.append(_initalThead());
                     $table.append(_initalTbody());
                     $this.empty().append($table);
                     $table.append(_initalTfoot());
-                } else {
-                    _initalTbody();
-                    _initalTfoot();
                 }
             };
-            _initTable(true);
+            _initTable();
         },
         exports: {},
         setOptionsBefore: null,
         setOptionsAfter: function(context) {
-            context._initTable(false);
+            context._initTable();
         },
         destroyBefore: function(context) {
             var $this = context.$element;
@@ -14021,17 +14034,22 @@ if (!Number.isNaN) {
         });
     });
 })(jQuery);
+
 //base on bs datetimepicker
 (function($) {
-    var inital = function() {
-        $('[data-picker]').each(function() {
-            var $this = $(this);
-            var type = $this.attr('data-picker');
+    var pickerContext = {
+        $element: null,
+        name: 'picker',
+        defaultOpt: null,
+        initBefore: null,
+        init: function(context) {
+            var $this = context.$element;
+            var type = context.opt.picker;
             var opt = {
                 todayBtn: true,
                 autoclose: true,
                 todayHighlight: true,
-                viewSelect: 4
+                viewSelect: 4,
             };
             switch (type) {
                 case 'date':
@@ -14059,13 +14077,25 @@ if (!Number.isNaN) {
                     break;
             }
             $this.datetimepicker(opt);
-            $this.removeAttr('data-picker');
-            $this.attr('role', 'Datepicker');
-        });
+        },
+        exports: {
+            original: function() {
+                return this.$element.data('datetimepicker');
+            }
+        },
+        isThirdPart: true,
+        setOptionsBefore: null,
+        setOptionsAfter: null,
+        destroyBefore: null,
+        initAfter: null,
     };
-
+    $.CUI.plugin(pickerContext);
     $(document).on('focus', '[data-picker]', function() {
-        inital();
+        var $this = $(this);
+        var opt = $this.data();
+        $this.picker(opt);
+        $this.removeAttr('data-picker');
+        $this.attr('role', 'Datepicker');
     });
 })(jQuery);
 
