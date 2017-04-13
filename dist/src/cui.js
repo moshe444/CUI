011012acf08dbf7ac132b8f231c149583a0203c7
Conjee Zou <shishiv30@live.cn>
2017-04-13 15:32:49 +0800

    Updates

--- dist/src/cui.js
+++ dist/src/cui.js
@@ -13847,8 +13847,8 @@ if (!Number.isNaN) {
             var $target = context.$target = $(opt.target);
 
             //record the traget's height
-            var height;
-            if ($target.offset().top < $this.offset().top) {
+            var height = 0;
+            if ($target.offset() && $target.offset().top < $this.offset().top) {
                 height = $target.height();
             }
 
@@ -15283,6 +15283,54 @@ if (!Number.isNaN) {
     });
 
 })(jQuery);
+(function($) {
+    $.fn.measurement = function(option) {
+        var defaultOption = {
+            data: [
+                {marker: '<div class="pin">A</div>', value: 100,}, {
+                    marker: '<div class="pin">B</div>',
+                    value: 500,
+                },
+                {marker: '<div class="pin">C</div>', value: 450,}
+            ],
+            min: null,
+            max: null,
+            onclick: null
+        };
+        var opt = $.extend({}, defaultOption, option);
+        var $this = $(this);
+        var _render = function() {
+            var $container = $('<div class="measurement"><div class="measurement-line"></div></div></div>');
+            $.each(opt.data, function(index, item) {
+                if (opt.min > item.value) {
+                    opt.min = item.value;
+                }
+                if (opt.max < item.value) {
+                    opt.max = item.value;
+                }
+            });
+            var total = opt.max - opt.min;
+            $.each(opt.data, function(index, item) {
+                var position = (item.value - opt.min ) / total * 100;
+                var $item = $('<div class="measurement-item">' + item.marker + '</div>');
+                $item.css({left: position + '%'});
+                $container.append($item);
+            });
+            var $min = $('<div class="measurement-min">' + opt.min + '</div>');
+            $container.append($min);
+            var $max = $('<div class="measurement-max">' + opt.max + '</div>');
+            $container.append($max);
+            return $container;
+        };
+        $this.append(_render());
+    };
+    $(document).on('dom.load.measurement', function() {
+        $('[data-measurement]').each(function(index, item) {
+            $(item).measurement($(item).data());
+            $(item).removeAttr('data-measurement');
+        });
+    });
+})(jQuery);
 //menu
 (function($) {
     $.fn.menu = function(option) {
@@ -15997,7 +16045,7 @@ if (!Number.isNaN) {
         var _getImageSize = function() {
             var maxHeight = $(window).height() - 100;
             var screenheight = opt.height > maxHeight ? maxHeight : opt.height;
-            var screenwidth = $this.width();
+            var screenwidth = $this.width() || $(window).width();
             var tmpWidth = screenwidth > opt.width ? opt.width : screenwidth - 2;
             var tmpHeight = tmpWidth * ratio;
             tmpHeight = screenheight > tmpHeight ? tmpHeight : screenheight;
